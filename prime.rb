c59b6acd16cc5e05f99b68963ab56892f5b58e53
Redmond Chan <redmondchan92@gmail.com>
2018-12-03 22:07:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(number)
+  check_pos = [2...number]
+  check_neg = [-2...number*(-1)]
+if check_pos.each do |divisor|
+  number%divisor < 0
+  return true
+end
+end
+end
