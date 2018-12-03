a5d3607cf6589b3eee3f9c5998be648f36c894e7
Redmond Chan <redmondchan92@gmail.com>
2018-12-03 20:24:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+  check = [(number*(-1))...number]
+  remainder = []
+  check.each do |divide|
+    remainder.push(check % number)
+  end
+    if remainder.include?(0)
+      return true
+    else
+      return false
+    end
+  end
